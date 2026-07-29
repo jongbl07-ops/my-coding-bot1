02711d44364b9fc896d83c3673a805181acfb3a0
jongbl07-ops <jong.bl07@gmail.com>
2026-07-29 16:01:24 +1200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import google.generativeai as genai
+from PIL import Image
+
+# ==========================================
+# 1. API 키 설정
+# ==========================================
+# 따옴표 안에 키를 직접 쓰지 말고, 아래처럼 'GEMINI_API_KEY'라는 이름표를 적어주세요!
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+
+genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel('gemini-2.5-flash')
